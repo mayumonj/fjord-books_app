6993fa469d8814d018080270b8c867246ac3bf72
mayumonj <>
2022-06-12 22:13:22 +0900

    削除しても動作に変化なかったため削除

--- config/initializers/locale.rb
+++
@@ -1,8 +0,0 @@
-# I18nライブラリに訳文の探索場所を指示する
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb, yml}')]
-
-# アプリケーションでの利用を許可するロケールのリストを渡す
-I18n.available_locales = %i[en ja]
-
-# ロケールを:en以外に変更する
-I18n.default_locale = :ja
