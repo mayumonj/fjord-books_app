a68ee266270210edfe54dcb45dda808b4c44e3d8
mayumonj <>
2022-06-18 12:56:44 +0900

    自分のアカウント情報の表示、編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if current_user.update(user_params)
+        format.html { redirect_to current_user, notice: t('controllers.common.notice_update', name: User.model_name.human) }
+        format.json { render :show, status: :ok, location: current_user }
+      else
+        format.html { render :edit }
+        format.json { render json: current_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :self_introduction, :postal_code, :address)
+  end
 end
