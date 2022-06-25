7947efd7e3df42b90c06fd3ced8b4741adb81b5a
mayumonj <>
2022-06-26 01:05:15 +0900

    Active Storageを使って画像をアップロード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validates :avatar, content_type: ['image/png', 'image/jpg', 'image/gif']
 end
