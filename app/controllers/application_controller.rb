e0e7ef7538ccfa86d01877d122044d35293e7050
Yatsu0720 <witdesigns@yatsu-world.com>
2022-08-06 08:52:32 +0900

    ユーザー一覧画面を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :seif_introduction]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :seif_introduction]) 
   end
 
 
