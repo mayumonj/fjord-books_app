c0af0461295f3214b3e6ef74abb12221075526d4
mayumonj <>
2022-08-14 16:04:01 +0900

    @userがnilになるケースの考慮をしなくていいように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: %i[show followees followers]
 
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 end
