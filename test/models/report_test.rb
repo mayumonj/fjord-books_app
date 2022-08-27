13b816ed93ac59a99882bf2a808a37865a468ca7
mayumonj <>
2022-08-27 14:30:36 +0900

    Reportクラスのインスタンスメソッドのテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    my_report = Report.new(title: 'Rails', content: 'テストを書き進めた。もう一息！', user: me)
+
+    assert_not my_report.editable?(she)
+    assert my_report.editable?(me)
+  end
+
+  test '#created_on' do
+    report = reports(:report_by_alice)
+    report.created_at = DateTime.new(2002, 8, 1, 12, 30, 45)
+    assert_equal Date.new(2002, 8, 1), report.created_on
+  end
 end
