ad325dd3bc394a54d1da1148693114c0d2d1f606
mayumonj <>
2022-09-01 00:23:21 +0900

    名前登録のないユーザーはemailを表示する処理をメソッドにまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,12 @@ class User < ApplicationRecord
     relationship = active_relationships.find_by(following_id: user.id)
     relationship&.destroy!
   end
+
+  def name_or_email
+    if name.empty?
+      email
+    else
+      name
+    end
+  end
 end
